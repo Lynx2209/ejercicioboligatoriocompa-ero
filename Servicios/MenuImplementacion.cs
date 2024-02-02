2d9dfda4e8e7884ea43a35eebd8d0ce4e78e75db
Lynx2209 <csi23-jconazo@colegioaltair.net>
2024-02-02 15:20:43 +0100

    Agregar archivos de proyecto.

---
+++ Servicios/MenuImplementacion.cs
@@ -0,0 +1,34 @@
+﻿using ejercicioboligatoriocompañero;
+using ejercicioboligatoriocompañero.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicioboligatoriocompañero.Servicios
+{
+    internal class MenuImplementacion : MenuInterfaz
+    {
+        public void Alumnocrear(List<dtosclase> alum)
+        {
+            dtosclase alumnocrear = new dtosclase();
+            Console.WriteLine("Escribe el nombre");
+            alumnocrear.Nombre = Console.ReadLine();
+            Console.WriteLine("Escribe el apellido");
+            alumnocrear.Apellido = Console.ReadLine();
+            Console.WriteLine("Escribe la clase");
+            alumnocrear.Clase = Console.ReadLine();
+            alum.Add(alumnocrear);
+        }
+
+        public void alumnocrear(List<dtosclase> alumnos)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+        
+    }
+
+}
---
+++ Servicios/MenuInterfaz.cs
@@ -0,0 +1,16 @@
+﻿using ejercicioboligatoriocompañero.Servicios;
+using ejercicioboligatoriocompañero.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicioboligatoriocompañero.Servicios
+{
+    internal interface MenuInterfaz
+    {
+        public void Alumnocrear(List<dtosclase> alum);
+        void alumnocrear(List<dtosclase> alumnos);
+    }
+}
